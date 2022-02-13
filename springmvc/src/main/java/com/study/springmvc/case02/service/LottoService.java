e5a58d080eee03418b4916bfe1185cbbd4ed05f4
vic <vichsu38@gmail.com>
2022-02-13 20:56:28 +0800

    Lotto

---
+++ springmvc/src/main/java/com/study/springmvc/case02/service/LottoService.java
@@ -0,0 +1,40 @@
+package com.study.springmvc.case02.service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoService {
+   private List<Set<Integer>> lottos = new ArrayList<>();
+ 
+   public  List<Set<Integer>> getLottos(){
+	 return lottos;
+   }
+   
+   public void addLotto() {
+	   lottos.add(generateLotto());
+   }
+   
+   public void updateLotto(int index) {
+	   lottos.set(index, generateLotto());
+   }
+ 
+   public void deleteLotto(int index) {
+	   lottos.remove(index);
+   }
+   
+   private Set<Integer> generateLotto(){
+	   Random r = new Random();
+	   //樂透 539: 1~39 取出不重複好碼
+	   Set<Integer> lotto = new LinkedHashSet<>();
+	   while(lotto.size()<5) {
+		   lotto.add(r.nextInt(39)+1);
+	   }
+	   return lotto;
+   }
+}
