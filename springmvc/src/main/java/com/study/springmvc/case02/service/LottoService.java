0ff779a7b64f56c1c1df41a262092156bb438774
vic <vichsu38@gmail.com>
2022-02-13 20:57:30 +0800

    lotto

--- springmvc/src/main/java/com/study/springmvc/case02/service/LottoService.java
+++ springmvc/src/main/java/com/study/springmvc/case02/service/LottoService.java
@@ -10,24 +10,30 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LottoService {
+   //總樂透	
    private List<Set<Integer>> lottos = new ArrayList<>();
- 
+   
+   //集合數字
    public  List<Set<Integer>> getLottos(){
 	 return lottos;
    }
    
+   //新增數字
    public void addLotto() {
 	   lottos.add(generateLotto());
    }
    
+   //更新數字
    public void updateLotto(int index) {
 	   lottos.set(index, generateLotto());
    }
  
+   //刪除數字
    public void deleteLotto(int index) {
 	   lottos.remove(index);
    }
    
+   //取號碼
    private Set<Integer> generateLotto(){
 	   Random r = new Random();
 	   //樂透 539: 1~39 取出不重複好碼
