d6c85fa977933b1f25faf03623cc2ab4263c5dd5
vic <vichsu38@gmail.com>
2022-02-26 10:26:52 +0800

    SPRING

--- springmvc/src/main/java/com/study/springmvc/lab/entity/Fund.java
+++ springmvc/src/main/java/com/study/springmvc/lab/entity/Fund.java
@@ -3,10 +3,16 @@ package com.study.springmvc.lab.entity;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class Fund {
-	private Integer fid;
+	private Integer fid; //基金
 	private String fname;
-	private Date createtime;
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8") // 返回時間類型
+	@DateTimeFormat(pattern="yyyy-MM-dd ") //接收時間類型
+    private Date createtime;
 	private List<FundStock> fundstocks; //單方
 
 	public Integer getFid() {
--- springmvc/target/classes/com/study/springmvc/lab/lab.sql
+++ springmvc/target/classes/com/study/springmvc/lab/lab.sql
@@ -5,7 +5,7 @@ create table if not exists fund(
  primary key(fid)
 );
 
-create table if not exists stock(
+create table if not exists fundstock(
  sid integer not null auto_increment,
  fid integer,
  symbol varchar(50),
@@ -13,3 +13,30 @@ create table if not exists stock(
  foreign key(fid) references fund(fid), -- 外鍵
  primary key(sid)
 );
+
+insert into fund(fname) values('A');
+insert into fund(fname) values('B');
+insert into fund(fname) values('C');
+insert into fund(fname) values('D');
+insert into fund(fname) values('E');
+insert into fund(fname) values('F');
+insert into fund(fname) values('G');
+
+insert into fundstock(fid , symbol , share ) values(1 , "2330.TW" , 500000);
+insert into fundstock(fid , symbol , share ) values(2 , "2330.TW" , 600000);
+insert into fundstock(fid , symbol , share ) values(2 , "2376.TW" , 700000);
+insert into fundstock(fid , symbol , share ) values(3 , "1101.TW" , 800000);
+insert into fundstock(fid , symbol , share ) values(3 , "2317.TW" , 900000);
+insert into fundstock(fid , symbol , share ) values(3 , "3231.TW" , 800000);
+insert into fundstock(fid , symbol , share ) values(4 , "2356.TW" , 700000);
+insert into fundstock(fid , symbol , share ) values(4 , "2880.TW" , 600000);
+insert into fundstock(fid , symbol , share ) values(4 , "2891.TW" , 500000);
+insert into fundstock(fid , symbol , share ) values(4 , "2330.TW" , 400000);
+insert into fundstock(fid , symbol , share ) values(5 , "2330.TW" , 300000);
+insert into fundstock(fid , symbol , share ) values(5 , "1101.TW" , 200000);
+insert into fundstock(fid , symbol , share ) values(5 , "2317.TW" , 100000);
+insert into fundstock(fid , symbol , share ) values(5 , "2886.TW" , 200000);
+insert into fundstock(fid , symbol , share ) values(5 , "2002.TW" , 300000);
+
+
+
